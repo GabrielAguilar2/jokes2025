a94b325a70508cd0adb2fafeb89a2b09c2c9340d
GabrielAguilar2 <gcaguilar234@gmail.com>
2026-02-10 20:25:20 +0000

    oki

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # Performance task ready
 
-import random 
+import random                                                                                  #Importing random allows random selections within the list
 
 joke_list = [
 ["Knock Knock", "Calder", "Calder police Ive been robbed!"],                                    # All three jokes are in this list
@@ -8,8 +8,9 @@ joke_list = [
 ["Knock Knock", "Broken pencil", "Nevermind it's pointless!"]
 ]
 
-ratings = []
-reviews = []
+                                                                                                 #TWO LISTS
+ratings = [1, 2, 6, 8, 3]                                                                        # ratings of the experience
+reviews = []                                                                                     # reviews of the experience
 
 
 def tell_joke(joke_list):
@@ -19,19 +20,19 @@ def tell_joke(joke_list):
         input("")
     
 def get_feedback(ratings, reviews):
-    rating = int(input("Okay! Please rate our jokes 1-10: "))                                        # after the user is finished they will give feedback
-    ratings.append(rating)
+    rating = int(input("Okay! Please rate your experience 1-10: "))                                    # after the user is finished they will give feedback
+    ratings.append(rating)                                                                             # rating is appended to the list
 
     review = input("Please type out a review with any praises or criticisms: ")
-    reviews.append(review)
+    reviews.append(review)                                                                             # review is appended to the list
 
-    average = sum(ratings) / len(ratings)
+    average = sum(ratings)/len(ratings)                                                                # THE AVERAGE RATING IS PRINTED
     print("___________________________________________")
 
     print("Thanks for your feedback!")
-    print("Average rating among all users: "), round(average, 1), "/ 10"
+    print("Average rating among all users: ", round(average, 1), "/ 10")
 
-    tell_friend = input("Will you tell any of your friends one of these jokes? ")
+    tell_friend = input("Will you tell any of your friends one of these jokes? ")                      # asks if the user will tell a friend about the jokes
     if tell_friend in ["yes","maybe","sure","yeah","of course","i will"]:
         print("Thanks, we appreciate it! ")
     else:
@@ -45,17 +46,17 @@ def get_feedback(ratings, reviews):
 
 # INPUT STARTS HERE
 
-answer = input("Do you want to hear a joke? ").lower()                                                                                       # Asks users if they want to hear a joke
-while answer =="yes":                                                                                                              # WHILE LOOP: when answer is yes it'll give them a random joke and then ask if tehy want another of if they're finished
+answer = input("Do you want to hear a joke or are you finished? ").lower()                                                                    # Asks users if they want to hear a joke
+while answer =="yes":                                                        # WHILE LOOP: when answer is yes it'll give them a random joke and then ask if tehy want another of if they're finished
         print("Cool! Let's do it! ")
         tell_joke(joke_list)
         answer = input("Do you want to hear another joke? ").lower()
 
-if answer in ["no", "finished","done","im done","im finished","nah"]:                                                                                                        # IF: They say finish then ask user for feedback
+if answer in ["finished","done","im done","im finished"]:                                               # IF They say finish then ask user for feedback
     get_feedback(ratings, reviews)
 
 else:
-     print("See you later!")                                                                                                   # ELSE: They say "no" or anythinng else for hearing a joke it will print "See you later"
+     print("See you later!")                                                        # ELSE They say "no" or anythinng else for hearing a joke it will print "See you later"
     
 
 
@@ -71,25 +72,6 @@ else:
 
 
 
-
-
-
-
-
-
-#EXTRA RANDOM CODE WE TRIED USING
-
-# like_jokes = input("Do you like jokes?")
-# while like_jokes == "yes":
-#     print("Well howdy!")
-#     print("I am quite the comidian")
-
-# else:
-#     double_check = ("Are you sure?")
-#     if double_check == "yes":
-#         print("Awww shucks, talk to you later :()")
-#     else:
-#         like_jokes == "yes"
 
 
 
